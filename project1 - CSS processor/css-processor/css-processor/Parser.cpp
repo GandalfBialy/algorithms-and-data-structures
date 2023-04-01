76e9f006b08bab8b7befea39bc256a8906f120f5
Igor Józefowicz <igorjozefowicz@gmail.com>
2023-04-01 21:12:14 +0200

    create Parser & CSS classes

---
+++ project1 - CSS processor/css-processor/css-processor/Parser.cpp
@@ -0,0 +1,9 @@
+#include "Parser.h"
+
+
+void Parser::loadCSS() {
+	List<String> lines;
+	
+	// read all lines from file
+	//while (getline)
+}
---
+++ project1 - CSS processor/css-processor/css-processor/Parser.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#include "List.h"
+#include "Node.h"
+#include "String.h"
+
+
+class Parser
+{
+private:
+	String line;
+
+public:
+	void loadCSS();
+};
+
