d3cd33ed2fa51c5bdca09db846aa9b1e76e78a4d
Igor Jozefowicz <igorjozefowicz@gmail.com>
2023-04-04 12:42:54 +0200

    develop CommandsInterpreter: parseCommand, executeCommand, getSelectorsCount, deletion commands

--- project1 - CSS processor/css-processor/css-processor/CommandsInterpreter.cpp
+++ project1 - CSS processor/css-processor/css-processor/CommandsInterpreter.cpp
@@ -13,10 +13,43 @@ CommandsInterpreter::CommandsInterpreter(CSS* css) {
 }
 
 
-Command CommandsInterpreter::parseCommand(String commandString) {
-	Command command = Command(commandString);
+void CommandsInterpreter::appendCommand(String commandString) {
+	Command command = parseCommand(commandString);
 
-	return command;
+	commands.append(command);
+}
+
+
+Command CommandsInterpreter::parseCommand(String commandName) {
+	int commasCount = commandName.countCharacter(',');
+	
+	if (commasCount == 2) {
+		String arguments[3];
+		int firstCommaIndex = commandName.findCharacter(',');
+		int secondCommaIndex = commandName.findCharacter(',', firstCommaIndex + 1);
+		int commandNameLength = commandName.getLength();
+
+		arguments[0] = commandName.substring(0, firstCommaIndex - 1);
+		arguments[1] = commandName.substring(firstCommaIndex + 1, secondCommaIndex - 1);
+		arguments[2] = commandName.substring(secondCommaIndex + 1, commandNameLength - 1);
+
+		return Command(commandName, arguments);
+	}
+
+	return Command(commandName);
+}
+
+
+void CommandsInterpreter::printCommands() {
+	std::cerr << "--- PRINTING COMMANDS ---" << std::endl;
+
+	for (int commandIndex = 0; commandIndex < commands.getSize(); commandIndex++) {
+		Command command = commands[commandIndex];
+		String commandName = command.getCommandName();
+		String* arguments = command.getArguments();
+
+		std::cerr << commandIndex << ".: " << commandName << "[" << arguments[0] << ";" << arguments[1] << ";" << arguments[2] << "]" << std::endl;
+	}
 }
 
 
@@ -35,43 +68,106 @@ void CommandsInterpreter::executeCommands() {
 
 void CommandsInterpreter::executeCommand(Command command) {
 	String commandName = command.getCommandName();
-	int commasCount = commandName.countCharacter(',');
+	String* arguments = command.getArguments();
 
 	std::cout << commandName << " == ";
 
-	if (commasCount == 0) {
-		if (commandName == "?") {
-			std::cout << getSectionsCount();
-		}
+	// ? (command)
+	if (commandName[0] == '?') {
+		std::cout << getSectionsCount() << "\n";
 	}
-	/*else if (commasCount == 2) {
-		if
-	}*/
+	// i,S,? OR z,S,? (command)
+	else if (arguments[1] == "S" and arguments[2] == "?") {
+		// i,S,? (command)
+		if (isArgumentNumber(arguments[0])) {
+			int sectionIndex = arguments[0].parseToInt() - 1;
+
+			std::cout << getSelectorsCount(sectionIndex) << "\n";
+
+			return;
+		}
+		
+		// z,S,? (command)
+		std::cout << getSelectorsCount(arguments[0]) << "\n";
+	}
+	// i,A,? (command)
+	else if (arguments[1] == "A" and arguments[2] == "?") {
+		int sectionIndex = arguments[0].parseToInt() - 1;
+
+		std::cout << getDeclarationsCount(sectionIndex) << "\n";
+	}
+	// i,A,n (command)
+	else if (arguments[1] == "A" and arguments[2].getLength() > 1) {
+		int sectionIndex = arguments[0].parseToInt() - 1;
+		String propertyName = arguments[2];
+
+		std::cout << getPropertyValue(sectionIndex, propertyName) << "\n";
+	}
+	// i,D,* OR i,D,n (command)
+	else if (arguments[1] == "D") {
+		// i,D,* (command)
+		if (arguments[2] == "*") {
+			int sectionIndex = arguments[0].parseToInt() - 1;
+
+			css->removeSection(sectionIndex);
+			std::cout << "deleted\n";
+
+			return;
+		}
+
+		
+		// i, D, n (command)
+		// delete attribute from i section and of name n
+		//int sectionIndex = arguments[0].parseToInt() - 1;
+		//String propertyName = arguments[2];
+		//
+		//// find property in declaration
+		//int declarationIndex = -1;
+		//
+		//for (int declarationIndex = 0; declarationIndex < css->getSections()[sectionIndex].getDeclarations().getSize(); declarationIndex++) {
+		//	if (css->getSections()[sectionIndex].getDeclarations()[declarationIndex].getProperty() == propertyName) {
+		//		break;
+		//	}
+		//}
+
+		//// delete property
+		//css->getSections()[sectionIndex].getDeclarations().removeAt(declarationIndex);
+
+		//// print section
+
+		//// if declaration is empty, delete it
+		//if (css->getSections()[sectionIndex].getDeclarations().getSize() == 0) {
+		//	css->removeSection(sectionIndex);
+		//}
+
+		//std::cout << "deleted\n";
+	}
 
 	std::cout << "\n";
 }
 
 
-void CommandsInterpreter::appendCommand(String commandString) {
-	Command command = parseCommand(commandString);
+bool CommandsInterpreter::isArgumentNumber(String argument) {
+	for (int characterIndex = 0; characterIndex < argument.getLength(); characterIndex++) {
+		if (argument[characterIndex] < '0' or argument[characterIndex] > '9') {
+			return false;
+		}
+	}
 
-	commands.append(command);
+	return true;
 }
+
+
+void CommandsInterpreter::setCSS(CSS* css) {
+	this->css = css;
+}
+
+
+// Commands
 
 
 int CommandsInterpreter::getSectionsCount() {
 	return css->getSectionsCount();
-}
-
-
-void CommandsInterpreter::printCommands() {
-	std::cerr << "--- PRINTING COMMANDS ---" << std::endl;
-	
-	for (int commandIndex = 0; commandIndex < commands.getSize(); commandIndex++) {
-		String commandName = commands[commandIndex].getCommandName();
-
-		std::cerr << commandIndex << ".: " << commandName << std::endl;
-	}
 }
 
 
@@ -82,7 +178,7 @@ int CommandsInterpreter::getSelectorsCount(int sectionIndex) {
 }
 
 
-int CommandsInterpreter::getDeclarationsCount(int sectionIndex, int selectorIndex) {
+int CommandsInterpreter::getDeclarationsCount(int sectionIndex) {
 	return css->getSections()[sectionIndex].getDeclarations().getSize();
 }
 
@@ -100,6 +196,31 @@ String CommandsInterpreter::getPropertyValue(int sectionIndex, String propertyNa
 }
 
 
-void CommandsInterpreter::setCSS(CSS* css) {
-	this->css = css;
+int CommandsInterpreter::getSelectorsCount(String selectorName) {
+	int selectorsCount = 0;
+	List<Section> sections = css->getSections();
+
+	for (Node<Section>* section = sections.getHead();; section = section->next) {
+		List<String> selectors = section->data.getSelectors();
+
+		//std::cerr << "Current section: " << section->data.getSectionName() << "\n";
+
+		for (Node<String>* selector = selectors.getHead();; selector = selector->next) {
+			//std::cerr << "Current selector: " << selector->data << "\n";
+
+			if (selector->data == selectorName) {
+				selectorsCount++;
+			}
+
+			if (selector->next == nullptr) {
+				break;
+			}
+		}
+
+		if (section->next == nullptr) {
+			break;
+		}
+	}
+
+	return selectorsCount;
 }
--- project1 - CSS processor/css-processor/css-processor/CommandsInterpreter.h
+++ project1 - CSS processor/css-processor/css-processor/CommandsInterpreter.h
@@ -17,22 +17,34 @@ public:
 	CommandsInterpreter();
 	CommandsInterpreter(CSS* css);
 
+	void appendCommand(String commandString);
 	Command parseCommand(String commandString);
+	void printCommands();
 
 	void executeCommands();
 	void executeCommand(Command command);
 
-	void appendCommand(String commandString);
+	bool isArgumentNumber(String argument);
 
-	int getSectionsCount(); // ? (command)
-	int getSelectorsCount(int sectionIndex); // i,S,? (command)
-	int getDeclarationsCount(int sectionIndex, int selectorIndex); // i,A,? (command)
-
-	String getPropertyValue(int sectionIndex, String propertyName); // i,A,n (command)
-
-	void printCommands();
-	
 	void setCSS(CSS* css);
+
+	// Commands
+	// ? (command)
+	int getSectionsCount(); 
+
+	// i,S,? (command)
+	int getSelectorsCount(int sectionIndex);
+	
+	// i,A,? (command)
+	int getDeclarationsCount(int sectionIndex); 
+
+
+	// i,A,n (command)
+	String getPropertyValue(int sectionIndex, String propertyName); 
+
+
+	// z,S,? (command
+	int getSelectorsCount(String selectorName);
 };
 
 /* COMMANDS TO COVER
@@ -48,6 +60,8 @@ i,A,? - wypisz liczbê atrybutów dla sekcji nr i, jeœli nie ma takiego bloku lub 
 
 i,S,j – wypisz j-ty selector dla i-tego bloku (numery sekcji oraz atrybutów zaczynaj¹ siê od 1) jeœli nie ma sekcji lub selektora pomiñ;
 (!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)
+implement blocks
+(!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)
 
 i,A,n – wypisz dla i-tej sekcji wartoœæ atrybutu o nazwie n, jeœli nie ma takiego pomiñ;
 [implemented, tests needed]
@@ -55,11 +69,14 @@ i,A,n – wypisz dla i-tej sekcji wartoœæ atrybutu o nazwie n, jeœli nie ma takieg
 n,A,? – wypisz ³¹czn¹ (dla wszystkich bloków) liczbê wyst¹pieñ atrybutu nazwie n. (W ramach pojedynczego bloku duplikaty powinny zostaæ usuniête na etapie wczytywania). Mo¿liwe jest 0;
 
 z,S,? – wypisz ³¹czn¹ (dla wszystkich bloków) liczbê wyst¹pieñ selektora z. Mo¿liwe jest 0;
+[implemented, tests needed]
 
 z,E,n – wypisz wartoœæ atrybutu o nazwie n dla selektora z, w przypadku wielu wyst¹pieñ selektora z bierzemy ostatnie. W przypadku braku pomiñ;
 
 i,D,* - usuñ ca³¹ sekcjê nr i (tj. separatory+atrybuty), po poprawnym wykonaniu wypisz deleted;
+[implemented, tests needed]
 
 i,D,n – usuñ z i-tej sekcji atrybut o nazwie n, jeœli w wyniku operacji pozostaje pusta sekcja powinna zostaæ równie¿ usuniêta (wraz z ew. selektorami), po poprawnym wykonaniu wypisz deleted.
+[implemented, tests needed]
 
 */
